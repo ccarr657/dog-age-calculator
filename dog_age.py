ab80101762869158ec4bb415bce704d9b518f58c
ccarr657 <65187272+ccarr657@users.noreply.github.com>
2025-04-10 13:01:56 -0400

    Add dog age calculator script

---
+++ dog_age.py
@@ -0,0 +1,9 @@
+# dog-age-calculator
+# Calculate dog age
+def dog_to_human_years(dog_age):
+    if dog_age <= 2>:
+        return 21 + (dog_age - 2) *4
+
+age = float(input("Enter your dog's age in years: "))
+human_age = dog_to_human_years(age)
+print(f"Your dog is about {human_age} human years old.")    
